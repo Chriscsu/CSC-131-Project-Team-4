01d99ed39d6552faba6871e50831d572ca1bde1d
Daniil Esir <152736136+daniilesir@users.noreply.github.com>
2025-02-28 20:03:03 -0800

    Updated Header
    Made color consistent across pages. Updated pages available on header.
    Navbar now goes away when clicked away.

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.hovercolor');
-
-menu.addEventListener('click', function () {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
+document.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.mobile-menu');
+    const menuLinks = document.querySelector('.hovercolor');
+    if (menu && menuLinks) {
+        menu.addEventListener('click', () => {
+            menu.classList.toggle('is-active');
+            menuLinks.classList.toggle('active');
+        });
+        document.addEventListener('click', (event) => {
+            if (!menu.contains(event.target) && !menuLinks.contains(event.target)) {
+                menu.classList.remove('is-active');
+                menuLinks.classList.remove('active');
+            }
+        });
+    }
 });
