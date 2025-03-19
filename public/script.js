a262e710cfefe423e76d86d5c2b6b9fb3812a60a
markpustyn <108764244+markpustyn@users.noreply.github.com>
2025-03-19 15:54:12 -0700

    trynna add event listener

--- public/script.js
+++ public/script.js
@@ -22,31 +22,12 @@ function clickAnswer(item) {
     answer.style.display = isVisible ? "none" : "block";
     arrow.style.transform = isVisible ? "rotate(0deg)" : "rotate(90deg)";
 }
-import { initializeApp } from "firebase/app";
-import { getAuth, connectAuthEmulator, signInWithEmailAndPassword } from 'firebase/auth'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAi4FZpvDGUcBvXCE25tAyRgusMSLFnEaE",
-  authDomain: "playground-218f9.firebaseapp.com",
-  databaseURL: "https://playground-218f9-default-rtdb.firebaseio.com",
-  projectId: "playground-218f9",
-  storageBucket: "playground-218f9.firebasestorage.app",
-  messagingSenderId: "571794642303",
-  appId: "1:571794642303:web:e3193939926c8adeb2b64a"
-};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
-
-connectAuthEmulator(auth, 'https://localhost:9099');
-const email = document.getElementById('email').value
-const password = document.getElementById('password').value
-console.log(email, password)
-
-const loginEmailPassword = async () => {
-    const loginEmail = email.value
-    const loginPassword = password.value
-    const userCred = await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
-    console.log(userCred.user)
+let submitBtn = document.getElementById("submitBtn")
+submitBtn.attachEvent('onclick', myFunctionReference);
+const myFunctionReference = function(){
+    alert("hello")
 }
+
+console.log("hello")
